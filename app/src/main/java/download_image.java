59ead4937140d9a3e76323e79f2552fd3e27227c
Dell <laibanaeem760@gmail.com>
2020-04-11 00:25:26 +0500

    hello

---
+++ app/src/main/java/download_image.java
@@ -0,0 +1,125 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.facebooklogin.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class download_page extends AppCompatActivity {
+
+    private Button back, imageDownloadBtn;
+    private EditText imagenameET;
+
+    private ImageView imageToDownloadIV;
+    private TextView gallery;
+    private String currentDate;
+
+    private ProgressBar bar;
+    private FirebaseFirestore objectFirebaseFirestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_image);
+        objectFirebaseFirestore = FirebaseFirestore.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+
+        back = findViewById(R.id.back);
+        objectFirebaseFirestore = FirebaseFirestore.getInstance();
+
+        connectXMLToJava();
+    }
+
+    private void connectXMLToJava() {
+        try {
+            imageToDownloadIV = findViewById(R.id.imageToDownloadIV);
+            imageDownloadBtn = findViewById(R.id.imageDownloadBtn);
+
+            imagenameET = findViewById(R.id.imageNameET);
+            gallery = findViewById(R.id.textgallery);
+
+            imagenameET.setText(currentDate);
+
+            bar = findViewById(R.id.ProgressBar);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    change();
+                }
+            });
+
+            imageDownloadBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    bar.setVisibility(View.VISIBLE);
+                    gallery.setVisibility(View.INVISIBLE);
+                    Download();
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(this, "connectXMLToJava:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void change() {
+        startActivity(new Intent(download_page.this, LoginPage.class));
+        finish();
+    }
+
+    private void Download() {
+        try {
+            if (!imagenameET.getText().toString().isEmpty()) {
+                bar.setVisibility(View.VISIBLE);
+                imageToDownloadIV.setImageResource(R.drawable.loading);
+                objectFirebaseFirestore.collection("Gallery")
+                        .document(imagenameET.getText().toString())
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()) {
+                                    bar.setVisibility(View.INVISIBLE);
+                                    String url = documentSnapshot.getString("URL");
+                                    Glide.with(download_page.this)
+                                            .load(url)
+                                            .into(imageToDownloadIV);
+                                    Toast.makeText(download_page.this, "Image Downloaded", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    bar.setVisibility(View.INVISIBLE);
+                                    Toast.makeText(download_page.this, "No Such File Exists", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        bar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(download_page.this, "Failed To Retrieve Image" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                bar.setVisibility(View.INVISIBLE);
+                Toast.makeText(this, "Please Enter Name of The Image To Download", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ex) {
+            bar.setVisibility(View.INVISIBLE);
+            Toast.makeText(this, "DownloadError: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
---
+++ app/src/main/java/login_page.java
@@ -0,0 +1,93 @@
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.facebooklogin.MainActivity;
+import com.example.facebooklogin.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginPage extends AppCompatActivity
+
+
+{
+
+    private Button Back;
+
+    TextView name, mail;
+    Button logout;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        Back = findViewById(R.id.back);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                change();
+            }
+        });
+
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+
+        mail = findViewById(R.id.mail);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (signInAccount != null) {
+            name.setText(signInAccount.getDisplayName());
+            mail.setText(signInAccount.getEmail());
+            Uri photo = signInAccount.getPhotoUrl();
+        }
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                try {
+                    GoogleSignIn.getClient(getApplicationContext(),
+                            new GoogleSignInOptions().Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+                    ).signOut();
+                } catch (Exception ex) {
+                    Toast.makeText(LoginPage.this, "Logging Out Error", Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void change() {
+        startActivity(new Intent(LoginPage.this, MainActivity.class));
+        finish();
+    }
+
+    public void openUploadPage(View view) {
+        try {
+            startActivity(new Intent(LoginPage.this, upload_image_page.class));
+        } catch (Exception e) {
+            Toast.makeText(this, "OpeningUploadPage:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void openDownloadPage(View view) {
+        try {
+            startActivity(new Intent(LoginPage.this, download_page.class));
+        } catch (Exception e) {
+            Toast.makeText(this, "OpeningDownloadPage:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
